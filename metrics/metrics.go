76e8333f714e1622c5f70a5c2636d8770222328b
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2025-06-10 22:08:54 +0200

    [nitpick] grammar
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -9,25 +9,25 @@ var (
 	// It is a Prometheus counter metric used for monitoring read operations on secrets.
 	SecretsRead = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "secrets_read",
-		Help: "Number of secrets that has been read.",
+		Help: "Number of secrets that have been read.",
 	})
 	// ExpiredSecretsRead counts the number of attempts to read expired secrets.
 	// This Prometheus counter helps monitor how often expired secrets are accessed.
 	ExpiredSecretsRead = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "expired_secrets_read",
-		Help: "Number of expired secrets that has been attempted to read.",
+		Help: "Number of expired secrets that have been attempted to read.",
 	})
 	// NonExistentSecretsRead counts the number of attempts to read secrets that do not exist.
 	// This Prometheus counter helps track how often clients try to access missing secrets.
 	NonExistentSecretsRead = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "nonexistent_secrets_read",
-		Help: "Number of non-existent secrets that has been attempted to read.",
+		Help: "Number of non-existent secrets that have been attempted to read.",
 	})
 	// SecretsCreated counts the total number of secrets that have been created.
 	// It is a Prometheus counter metric used for monitoring secret creation events.
 	SecretsCreated = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "secrets_created",
-		Help: "Number of secrets that has been created.",
+		Help: "Number of secrets that have been created.",
 	})
 	// SecretsCreatedWithError counts the number of failed attempts to create a secret.
 	// This Prometheus counter is incremented each time a secret creation operation fails.
@@ -39,6 +39,6 @@ var (
 	// This Prometheus counter is used to track deletion events for monitoring purposes.
 	SecretsDeleted = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "secrets_deleted",
-		Help: "Number of secrets that has been deleted.",
+		Help: "Number of secrets that have been deleted.",
 	})
 )
